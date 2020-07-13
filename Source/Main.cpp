e47751f90bf9b253d334a417a898f09d1a86fcd6
Jercan Alex <jercan_alex27@yahoo.com>
2020-07-13 12:58:02 +0300

    Created openCL project

---
+++ Source/Main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <memory.h>
+#include <vector>
+
+#include "CL\cl.h"
+
+//for perf. counters
+#include <Windows.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello world!");
+	
+	return 0;
+}
